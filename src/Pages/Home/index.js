a0c2488a197dd661c2bc7e0a68edf70135cb86cb
Truong <duytruong5437@gmail.com>
2022-08-31 10:36:05 +0700

    add checkbox

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+
+import HomeBanner from './components/HomeBanner';
+import HomeWork from './components/HomeWork';
+import HomeIngredient from './components/HomeIngredient';
+import HomeCategory from './components/HomeCategory';
+import HomeDelivery from './components/HomeDelivery';
+import HomeProduct from './components/HomeProduct';
+import HomeAnalyst from './components/HomeAnalyst';
+import HomeReview from './components/HomeReview';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+       <HomeBanner />
+       <HomeWork />
+       <HomeIngredient />
+       <HomeCategory />
+       <HomeDelivery />
+       <HomeProduct />
+       <HomeAnalyst />
+       <HomeReview />
+    </>
   )
 }
 
