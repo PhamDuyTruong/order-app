5969a3bf4dc4469c8958084274689f6f9a43052a
Truong <duytruong5437@gmail.com>
2022-08-31 16:13:05 +0700

    add product component

---
+++ src/Components/FoodProducts/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import "./styles.scss";
+import { useHistory, useParams } from 'react-router-dom';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+
+import StarIcon from '@material-ui/icons/Star';
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import RoomIcon from '@material-ui/icons/Room';
+
+const FoodProducts = (props) => {
+    const { id, name, img, dsc, price, rate, country, openDialog, moveToTop } = props;
+    const params = useParams();
+    const history = useHistory();
+
+  return (
+    <div id={id} className="shop-product">
+        <div className='shop-product__img-wrapper'>
+            <LazyLoadImage  
+               effect='blur'
+               src={img}
+               className="shop=product__img"
+               alt={name}
+               width="100%"
+               height="100%"
+            >
+            </LazyLoadImage>
+            <div className='shop-product__rate'>
+                <StarIcon />
+                <span>{rate}</span>
+            </div>
+        </div>
+
+        <div className='shop-product__content'>
+            <div className='shop-product__name'>{name}</div>
+            <p className='shop-product__description'>{dsc}</p>
+            <div className='shop-product__row'>
+                <div className='shop-product__location'>
+                    <RoomIcon />
+                    <span>{country}</span>
+                </div>
+                <div className='shop-product__price'>$ {price}</div>
+            </div>
+        </div>
+
+        <div className='shop-product__btns'>
+            <div className='shop-product__btn'>
+                <FavoriteBorderIcon />
+            </div>
+            <div className='shop-product__btn'>
+                <ShoppingCartOutlinedIcon />
+            </div>
+        </div>
+        <div className='shop-product__label'>Favourite</div>
+    </div>
+  )
+}
+
+export default FoodProducts
