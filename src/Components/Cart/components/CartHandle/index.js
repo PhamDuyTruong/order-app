1e7a0412f813817eb3e82cacd7d827b3e6a4ee39
Truong <duytruong5437@gmail.com>
2022-09-04 21:25:33 +0700

    add button component

--- src/Components/Cart/components/CartHandle/index.js
+++ src/Components/Cart/components/CartHandle/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import StoreMallDirectoryIcon from "@material-ui/icons/StoreMallDirectory";
 
+import PrimaryButton from '../../../PrimaryButton';
+import "./styles.scss";
+
 const CartHandle = () => {
+   const [isActive, setIsActive] = useState(false);
+   const toggleDropUp = () =>{
+      setIsActive(!isActive);
+   };
+
   return (
     <div className='cart-handle'>
-       <div className='cart-handle__dropup'></div>
-       <div className='cart-handle__detail'>
+       <div onClick={toggleDropUp} className='cart-handle__dropup'></div>
+       <div className={ isActive ? "cart-handle__detail active" : "cart-handle__detail"}>
           <h3 className="cart-handle__detail-title">Information</h3>
           <div className='cart-handle__row'>
              <span className='cart-handle__label'>Discount</span>
@@ -28,6 +37,21 @@ const CartHandle = () => {
 
        <div className='cart-handle__btns'>
            {/* Primary Button */}
+           <PrimaryButton
+              page="checkout"
+              subClass="red cart-handle__btn"
+              className="cart-handle__btn cart-handle__btn--checkout"
+           >
+               <ShoppingCartIcon />
+               <span>Checkout</span>
+           </PrimaryButton>
+           <PrimaryButton
+              page="shop"
+              subClass="cart-handle__btn"
+           >
+               <StoreMallDirectoryIcon />
+                <span>Buy more</span>
+           </PrimaryButton>
        </div>
     </div>
   )
