e52b7ff790c5ad07183568ab5bbf2c075c183687
Truong <duytruong5437@gmail.com>
2022-09-04 19:39:29 +0700

    add primary-button

--- src/Components/Cart/index.js
+++ src/Components/Cart/index.js
@@ -1,18 +1,39 @@
-import React from 'react';
-
+import React, {useContext, useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {db} from '../../config/firebaseConfig';
+import {addToCart} from './cartSlice';
+import {setIsShowCart} from '../Header/headerSlice';
 import CartItems from './components/CartItem';
 import CartHandle from './components/CartHandle';
-import "./styles.scss"
+import EmptyCart from '../EmptyCart';
+import EmptyCartImg from '../../assets/images/empty-cart.svg'
+import "./styles.scss";
 
 const Cart = () => {
+  const {isShowCart} = useSelector((state) => state.header);
+  const cartProducts = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  const closeCart = () =>{
+    const action = setIsShowCart(false);
+    dispatch(action);
+  };
+
+  useEffect(() =>{
+
+  })
+
   return (
-    <div className='cart'>
-        <div className='cart__overlay'></div>
+    <div className={isShowCart ? 'cart active' : 'cart'}>
+        <div onClick={closeCart} className='cart__overlay'></div>
         <div className='cart__container'>
             <div className='cart__heading'>
                 <h2>Shopping Cart</h2>
-                <div className='cart__close'></div>
+                <div className={!isShowCart ? 'cart__close active' : 'cart__close'} onClick={closeCart}></div>
             </div>
+            {cartProducts.length < 0 && (
+              <EmptyCart src={EmptyCartImg} type="shop"/>
+            )}
             <CartItems />
             <CartHandle />
         </div>
