640c74a9291c0b5ea2026dab7f62d3879f4516dd
Truong <duytruong5437@gmail.com>
2022-09-05 19:27:49 +0700

    add hook

--- src/Components/Dialog/index.js
+++ src/Components/Dialog/index.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import { Button } from "@material-ui/core";
-import "./styles.scss"
+import "./styles.scss";
+import { useHistory } from "react-router-dom";
 
-const Dialog = () => {
+const Dialog = (props) => {
+  const {setIsShow, isShow} = props;
+  const history = useHistory();
+
+  const hideDialog = () =>{
+    setIsShow(false);
+  };
+
+  const moveToLogin = () =>{
+    setIsShow(false);
+    history.push('/login')
+  }
   return (
-    <div className='dialog'>
-        <div className='dialog__overlay'></div>
+    <div className={isShow ? "dialog show" : "dialog"}>
+        <div onClick={hideDialog} className='dialog__overlay'></div>
         <div className='dialog__wrapper'>
             <h2 className='dialog__title'>Join with us</h2>
             <p className='dialog__description'>
                 Please sign in to use this our features 
             </p>
             <div className='dialog__btns'>
-                <Button>Cancel</Button>
+                <Button onClick={hideDialog} className="dialog__btn">Cancel</Button>
                 <Button
                    variant="contained"
                    color="primary"
                    className="dialog__btn"
+                   onClick={moveToLogin}
                 >
                     Log in
                 </Button>
--- src/hooks/useFirestoreProducts.js
+++ src/hooks/useFirestoreProducts.js
@@ -2,5 +2,77 @@ import {db} from '../config/firebaseConfig';
 
 function useFirestoreProducts(){
     const query = db.collection("users");
-    
-}
+    const checkProductExists = (data, productInfo) =>{
+       return data.some((item) => item.id === productInfo.id);
+    }
+    const handleAddToCart = (data, productInfo, action) =>{
+        const isProductExists = checkProductExists(data, productInfo);
+        if(isProductExists){
+            const index = data.findIndex((item) => item.id === productInfo.id);
+            const productQnt = data[index].qnt;
+            const updateProduct = {
+                ...data[index],
+                qnt: action === "increase" ? productQnt + 1 : action === "decrease" ? productQnt - 1 || 1 : productQnt + productInfo.qnt || 1,
+            };
+            data[index] = updateProduct;
+            return data;
+        }else{
+            return data.concat({...productInfo, qnt: productInfo.qnt || 1});
+        }
+    };
+
+    const addToFirestore = (uid, product) =>{
+        query.doc(uid).get().then((doc) =>{
+            if(doc.exists){
+                const {type, productInfo, action} = product || "";
+                const cartData = doc.data().cart;
+                const wishlistData = doc.data().wishlist;
+                productInfo && db.collection("users").doc(uid).set({
+                    cart: type==="success" ? handleAddToCart(cartData, productInfo, action) : cartData,
+                    wishlist: type === "wishlist" && !checkProductExists(wishlistData, productInfo) ? wishlistData.concat(productInfo) : wishlistData
+                })
+            }else{
+                query.doc(uid).set({
+                    cart: [],
+                    wishlist: []
+                })
+            }
+        }).catch((e) =>{
+            console.log("failed: ", e.message);
+        })
+    };
+
+    const removeFromFirestore = (uid, product, option) =>{
+        query.doc(uid).get().then((doc) =>{
+            const {type, productInfo} = product;
+            const cartData = doc.data().cart;
+            const wishlistData = doc.data().wishlist;
+
+            const index = (type === "success" ? cartData: wishlistData).findIndex((item) => item.id === productInfo.id);
+            type=== "success" ? cartData.splice(index, 1) : wishlistData.splice(index, 1);
+            productInfo && query.doc(uid).set({
+                cart: cartData,
+                wishlist: wishlistData
+            })
+        }).catch((e) => {
+            console.log(e.message);
+        })
+    };
+
+    const removeAllFromFirestore = (uid) =>{
+       query.doc(uid).get().then((doc) =>{
+         const cartData = doc.data().cart;
+         const wishlistData = doc.data().wishlist;
+
+         cartData.length = 0;
+         query.doc(uid).set({
+            cart: cartData,
+            wishlist: wishlistData
+         })
+       })
+    };
+
+    return { addToFirestore, removeFromFirestore, removeAllFromFirestore };
+};
+
+export default useFirestoreProducts;
