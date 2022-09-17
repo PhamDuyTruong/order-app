cb339ea004a1d5c6c349252242e28626064b5904
Truong <duytruong5437@gmail.com>
2022-09-17 22:15:36 +0700

    add empty shop

--- src/Pages/Shop/components/ShopContent/ShopEmpty.js
+++ src/Pages/Shop/components/ShopContent/ShopEmpty.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import "./ShopEmpty.scss"
+import "./ShopEmpty.scss";
+
+import HandleImage from '../../../../Components/Banner/HandleImage';
+import EmptyShopImg from '../../../../assets/images/empty-shop.svg';
+
 
 const ShopEmpty = () => {
   return (
-    <div>ShopEmpty</div>
+    <div className='shop-empty'>
+         <img src={HandleImage(EmptyShopImg)} alt="Empty-shop-img"/>
+         <h2 className='shop-empty__title'>
+             There is no product you looking for
+         </h2>
+    </div>
   )
 }
 
--- src/Pages/Shop/components/ShopContent/index.js
+++ src/Pages/Shop/components/ShopContent/index.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
 import {useSelector} from 'react-redux'
 import "./style.scss";
-import ShopProducts from './ShopProducts'
+import ShopProducts from './ShopProducts';
+import ShopHandle from './ShopHandle';
 
 
 const ShopContent = () => {
