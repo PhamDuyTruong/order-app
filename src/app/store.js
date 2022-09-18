f7292a730e2a4406fdc32be179160dd49b240a14
Truong <duytruong5437@gmail.com>
2022-09-18 11:26:19 +0700

    add detail slice

--- src/app/store.js
+++ src/app/store.js
@@ -3,13 +3,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import headerReducer from '../Components/Header/headerSlice';
 import wishlistReducer from '../Components/Wishlist/wishlistSlice';
 import cartReducer from '../Components/Cart/cartSlice';
-import shopReducer from '../Pages/Shop/shopSlice'
+import shopReducer from '../Pages/Shop/shopSlice';
+import detailReducer from '../Pages/Detail/detailSlice'
 
 const rootReducer = {
     header: headerReducer,
     wishlist: wishlistReducer,
     cart: cartReducer,
-    shop: shopReducer
+    shop: shopReducer,
+    detail: detailReducer
 };
 
 const store = configureStore({
