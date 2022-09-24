1d12d033ae5c8d3aea4904d4a524b45deed6936a
Truong <duytruong5437@gmail.com>
2022-09-24 11:33:01 +0700

    add checkout component

---
+++ src/Pages/Checkout/components/CheckoutContent/index.js
@@ -0,0 +1,66 @@
+import React, {useState} from 'react';
+import {useSelector} from "react-redux";
+import "./style.scss";
+
+import {BackgroundIconBlurOne, BackgroundIconBlurTwo, BackgroundIconBlurThree, BackgroundIconBlurFour} from '../../../../utils/BgIcons';
+import BgIcon from '../../../../Components/BgIcon';
+
+
+const CheckoutContent = () => {
+    const [isCheckoutSuccess, setIsCheckoutSuccess] = useState(false);
+    const [isBuy, setIsBuy] = useState(false);
+    const cartProducts = useSelector((state) => state.cart);
+
+
+  return (
+    <div className='checkout-content'>
+        <div className='checkout-content__left'>
+            
+
+        </div>
+        <div className='checkout-content__right' style={{ paddingTop: cartProducts.length > 0 ? "95px" : "60px" }}>
+        <BgIcon
+          src={BackgroundIconBlurOne}
+          width="25"
+          top="25"
+          right="-30"
+          type="float"
+          duration="3"
+          zIndex="0"
+        />
+        <BgIcon
+          src={BackgroundIconBlurTwo}
+          width="18"
+          bottom="30"
+          left="5"
+          type="float"
+          duration="4"
+          zIndex="0"
+          delay="1"
+        />
+        <BgIcon
+          src={BackgroundIconBlurThree}
+          width="20"
+          bottom="5"
+          right="40"
+          type="scale"
+          duration="6"
+          zIndex="0"
+          delay="2"
+        />
+        <BgIcon
+          src={BackgroundIconBlurFour}
+          width="12.5"
+          top="5"
+          left="30"
+          type="scale"
+          duration="5"
+          zIndex="0"
+          delay="1"
+        />
+        </div>
+    </div>
+  )
+}
+
+export default CheckoutContent
